0a0676e5c73660ba1ba5fb779269cce811193b38
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-10-23 20:27:19 +0200

    Add project files.

---
+++ Kappitel 4 uppgift 2.28/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in din mening");
+            string mening = Console.ReadLine();
+            Console.WriteLine("Skriv in två bokstäver");
+            string bok = Console.ReadLine();
+ 
+
+            if (mening.Contains(bok))
+            {
+                Console.WriteLine("Din mening innerhåller dinna två ord");
+            }
+            else
+            {
+                Console.WriteLine("Din mening innerhåller inte dinna två ord");
+            }
+           
+
+        }
+    }
+}
+
